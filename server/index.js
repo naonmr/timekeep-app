f03dbce7ea134b0a41292a8fe16de69249e207bf
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-04 23:33:22 +0900

    feat: create get user info API

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
 const { Prisma, PrismaClient } = require("@prisma/client");
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "..", "build")));
 
-async function main() {
-  // ... you will write your Prisma Client queries here
+app.get("/users", async (req, res) => {
   const allUsers = await prisma.user.findMany();
   console.log(allUsers);
-}
+  res.json(allUsers);
+});
 
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
