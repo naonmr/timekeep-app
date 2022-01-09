05c17ec4dd51c9b602ae1560eb448e9f1ba48f79
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-09 15:55:05 +0900

    init/ setup firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGEING_SENDER_ID,
+  appId: process.env.FIREBASE_APPID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
