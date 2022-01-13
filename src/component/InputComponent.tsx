de02a8fcf611adfd1c09c3ba019e0f4b0210e59a
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-13 16:02:35 +0900

    style: reflect chakra

--- src/component/InputComponent.tsx
+++ src/component/InputComponent.tsx
@@ -6,6 +6,11 @@ import {
   FormControl,
   Input,
   Button,
+  Stack,
+  Center,
+  Flex,
+  InputGroup,
+  InputRightAddon,
 } from "@chakra-ui/react";
 
 type Contents = {
@@ -42,59 +47,95 @@ const InputComponent: React.VFC = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("meetingTitle")} placeholder="Meeting title" />
+      <Center>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* input部分 */}
+          <div className="agendaForm">
+            <Input
+              variant="filled"
+              {...register("meetingTitle")}
+              placeholder="Meeting title"
+            />
 
-        {fields.map((field, index) => {
-          return (
-            <div key={field.id}>
-              <section className={"section"} key={field.id}>
-                <input
-                  placeholder="name"
-                  {...register(`agendas.${index}.agenda` as const, {
-                    required: true,
-                  })}
-                  className={errors?.agendas?.[index]?.agenda ? "error" : ""}
-                  defaultValue={field.agenda}
-                  onFocus={() => setFocusIndex(index)}
-                />
-                <input
-                  placeholder="time"
-                  type="number"
-                  {...register(`agendas.${index}.time` as const, {
-                    valueAsNumber: true,
-                    required: true,
-                  })}
-                  className={errors?.agendas?.[index]?.time ? "error" : ""}
-                  defaultValue={field.time}
-                  onFocus={() => setFocusIndex(index)}
-                />
-                <button type="button" onClick={() => remove(index)}>
-                  Delete
-                </button>
-              </section>
-            </div>
-          );
-        })}
-        <button
-          type="button"
-          onClick={() => {
-            append({ agenda: "", time: 0 });
-          }}
-        >
-          add
-        </button>
-        <button
-          type="button"
-          onClick={() => insert(focusIndex, { agenda: "", time: 0 })}
-        >
-          insert
-        </button>
-        <button type="button" onClick={() => move(focusIndex, focusIndex + 1)}>
-          move
-        </button>
-        <input type="submit" />
-      </form>
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id}>
+                  <section className={"section"} key={field.id}>
+                    <Flex>
+                      <Input
+                        variant="outline"
+                        placeholder="name"
+                        {...register(`agendas.${index}.agenda` as const, {
+                          required: true,
+                        })}
+                        className={
+                          errors?.agendas?.[index]?.agenda ? "error" : ""
+                        }
+                        defaultValue={field.agenda}
+                        onFocus={() => setFocusIndex(index)}
+                      />
+                      <InputGroup>
+                        <Input
+                          variant="outline"
+                          placeholder="time"
+                          type="number"
+                          {...register(`agendas.${index}.time` as const, {
+                            valueAsNumber: true,
+                            required: true,
+                          })}
+                          className={
+                            errors?.agendas?.[index]?.time ? "error" : ""
+                          }
+                          defaultValue={field.time}
+                          onFocus={() => setFocusIndex(index)}
+                        />
+                        <InputRightAddon children="min" />
+                      </InputGroup>
+                      <Button type="button" onClick={() => remove(index)}>
+                        Delete
+                      </Button>
+                    </Flex>
+                  </section>
+                </div>
+              );
+            })}
+          </div>
+
+          <Center>
+            <Stack spacing={2} direction="row" align="center">
+              <Button
+                colorScheme="teal"
+                variant="outline"
+                type="button"
+                onClick={() => {
+                  append({ agenda: "", time: 0 });
+                }}
+              >
+                add
+              </Button>
+              <Button
+                colorScheme="teal"
+                variant="outline"
+                type="button"
+                onClick={() => insert(focusIndex, { agenda: "", time: 0 })}
+              >
+                insert
+              </Button>
+              <Button
+                colorScheme="teal"
+                variant="outline"
+                type="button"
+                onClick={() => move(focusIndex, focusIndex + 1)}
+              >
+                move
+              </Button>
+            </Stack>
+          </Center>
+          <Button colorScheme="teal" variant="solid" type="submit" m={2}>
+            そうしん！
+          </Button>
+        </form>
+      </Center>
     </>
   );
 };
