07c261f5369c03eafb2cac48147797048cc28a20
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-22 23:52:45 +0900

    bugfix: seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,31 +3,34 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const userData1 = {
-  uid: "d0n4OWQHCfVVmHPApeNUZ1yU3672",
-  name: "test",
-  meetings: {
-    create: [
-      {
-        title: "test1",
-        agendas: {
-          create: [
-            { order: 1, title: "agenda1", time: 1 },
-            { order: 2, title: "agenda2", time: 2 },
-          ],
+  data: {
+    uid: "d0n4OWQHCfVVmHPApeNUZ1yU3672",
+    name: "test",
+    meetings: {
+      create: [
+        {
+          title: "test1",
+          agendas: {
+            create: [
+              { order: 1, title: "agenda1", time: 1 },
+              { order: 2, title: "agenda2", time: 2 },
+            ],
+          },
         },
-      },
-      {
-        title: "test2",
-        agendas: {
-          create: [
-            { order: 1, title: "agenda2-1", time: 1 },
-            { order: 2, title: "agenda2-1", time: 2 },
-          ],
+        {
+          title: "test2",
+          agendas: {
+            create: [
+              { order: 1, title: "agenda2-1", time: 1 },
+              { order: 2, title: "agenda2-1", time: 2 },
+            ],
+          },
         },
-      },
-    ],
+      ],
+    },
   },
 };
+
 const userData2 = {
   uid: "61Q9QHo01HNJ66IP0MCOHSbIvd43",
   name: "testdesu",
@@ -37,8 +40,8 @@ const userData2 = {
         title: "testdesu1",
         agendas: {
           create: [
-            { title: "agendadesu1", time: 1 },
-            { title: "agendadesu2", time: 2 },
+            { order: 1, title: "agendadesu1", time: 1 },
+            { order: 2, title: "agendadesu2", time: 2 },
           ],
         },
       },
@@ -46,8 +49,8 @@ const userData2 = {
         title: "testdesu2",
         agendas: {
           create: [
-            { title: "agendadesu2-1", time: 1 },
-            { title: "agendadesu2-1", time: 2 },
+            { order: 1, title: "agendadesu2-1", time: 1 },
+            { order: 2, title: "agendadesu2-1", time: 2 },
           ],
         },
       },
@@ -56,14 +59,10 @@ const userData2 = {
 };
 
 async function seed() {
-  const user1 = await prisma.user.create({
-    data: userData1,
-  });
-  const user2 = await prisma.user.create({
-    data: userData2,
-  });
+  const user1 = await prisma.user.create(userData1);
+  // const user2 = await prisma.user.create(userData2);
   console.log(`Created user with id: ${user1.id}`);
-  console.log(`Created user with id: ${user2.id}`);
+  // console.log(`Created user with id: ${user2.id}`);
   console.log(`Seeding finished.`);
 }
 
