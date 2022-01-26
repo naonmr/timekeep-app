a1035162c246939bfad53bb195cb7218c57848a0
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-26 23:28:51 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import TimerPage from "./pages/TimerPage";
 
 import PrivateRoute from "./firebase/PrivateRoute";
 import { AuthProvider } from "./firebase/AuthContext";
-import { BrowserRouter, Switch, Route, Router } from "react-router-dom";
+import { BrowserRouter, Switch, Route, HashRouter } from "react-router-dom";
 import IsRegister from "./pages/IsRegister";
 import Home from "./pages/Home";
 
@@ -17,9 +17,9 @@ const App: React.FC = () => {
   return (
     <>
       <AuthProvider>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename="/">
           <Switch>
-            <Route children={<Home />} />
+            <Route exact path="/" children={<Home />} />
             <Route path="/signup" component={SignUp} />
             <Route path="/is-register" component={IsRegister} />
             <Route path="/login" component={Login} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import { useHistory } from "react-router-dom";
+import { PrimaryButton } from "../component/Button";
+
 const Home = () => {
+  const history = useHistory();
   return (
     <>
       <h1>HOME!</h1>
+      <PrimaryButton text="login" onclick={() => history.push("/login")} />
     </>
   );
 };
