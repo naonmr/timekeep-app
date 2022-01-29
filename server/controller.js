4f47bde1291f0573f34647c2562d3e7a78ec7d5f
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-29 13:31:08 +0900

    refactor: remove console.log & separate index.js and  controller.js

---
+++ server/controller.js
@@ -0,0 +1,147 @@
+export const postNewUser = async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  const newUser = await prisma.user.create({ data: data });
+  res.json(newUser);
+};
+
+export const getMeetings = async (req, res) => {
+  const uid = req.params.uid;
+  const meetings = await prisma.meeting.findMany({
+    where: {
+      authorId: uid,
+    },
+  });
+  // getした情報を並び替える;
+  for (let outer = 0; outer < meetings.length - 1; outer++) {
+    for (let i = meetings.length - 1; i > outer; i--) {
+      if (meetings[i].id < meetings[i - 1].id) {
+        let tmp = meetings[i];
+        meetings[i] = meetings[i - 1];
+        meetings[i - 1] = tmp;
+      }
+    }
+  }
+  res.json(meetings);
+};
+
+export const postMeeting = async (req, res) => {
+  const data = req.body;
+  const uid = req.params.uid;
+
+  let count = 1;
+  const agendas = data.agendas.map((agenda) => {
+    agenda.order = count;
+    count++;
+    return agenda;
+  });
+
+  let newMeeting = {
+    title: data.title,
+    author: {
+      connect: {
+        uid: uid,
+      },
+    },
+    agendas: {
+      create: agendas,
+    },
+  };
+  const createMeeting = await prisma.meeting.create({ data: newMeeting });
+  res.json(createMeeting);
+};
+
+export const deleteMeeting = async (req, res) => {
+  const meetingId = Number(req.query.meetingId);
+
+  const deleteAgenda = await prisma.agenda.deleteMany({
+    where: {
+      meetingId: meetingId,
+    },
+  });
+  const deleteMeeting = await prisma.meeting.delete({
+    where: {
+      id: meetingId,
+    },
+  });
+
+  res.json(deleteAgenda);
+};
+
+export const putMeeting = async (req, res) => {
+  const meetingId = Number(req.query.meetingId);
+  const data = req.body;
+
+  let count = 1;
+  const agendas = data.agendas.map((agenda) => {
+    agenda.order = count;
+    count++;
+    return agenda;
+  });
+
+  const deleteAgenda = await prisma.agenda.deleteMany({
+    where: {
+      meetingId: meetingId,
+    },
+  });
+
+  const putMeeting = await prisma.meeting.update({
+    where: {
+      id: meetingId,
+    },
+    data: { title: data.title },
+  });
+
+  data.agendas.map(async (agenda) => {
+    const createNewAgenda = await prisma.agenda.create({
+      data: {
+        title: agenda.title,
+        time: agenda.time,
+        order: agenda.order,
+        meeting: {
+          connect: {
+            id: meetingId,
+          },
+        },
+      },
+    });
+  });
+
+  res.json(putMeeting);
+};
+
+export const getAgendas = async (req, res) => {
+  const meetingId = Number(req.query.meetingId);
+
+  const meetingInfo = await prisma.meeting.findUnique({
+    where: {
+      id: meetingId,
+    },
+  });
+
+  if (meetingInfo) {
+    let agendas = await prisma.agenda.findMany({
+      where: {
+        meetingId: meetingId,
+      },
+    });
+
+    if (agendas.length > 0) {
+      // getした情報を並び替える;
+      for (let outer = 0; outer < agendas.length - 1; outer++) {
+        for (let i = agendas.length - 1; i > outer; i--) {
+          if (agendas[i].order < agendas[i - 1].order) {
+            let tmp = agendas[i];
+            agendas[i] = agendas[i - 1];
+            agendas[i - 1] = tmp;
+          }
+        }
+      }
+      const resData = { title: meetingInfo.title, agendas: agendas };
+      res.status(200).json(resData);
+    }
+  }
+  if (!meetingInfo) {
+    res.status(403).json("not auther");
+  }
+};
--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const { title } = require("process");
+const {
+  postNewUser,
+  getMeetings,
+  postMeeting,
+  deleteMeeting,
+  putMeeting,
+  getAgendas,
+} = require("./controller");
 
 const prisma = new PrismaClient();
 
@@ -13,163 +21,15 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "..", "build")));
 
-app.post("/api/user/:uid", async (req, res) => {
-  const data = req.body;
-  console.log(data);
-  const newUser = await prisma.user.create({ data: data });
-  res.json(newUser);
-});
+app.get("/api/meetings/:uid", getMeetings);
+app.get("/api/agendas/:uid", getAgendas);
 
-app.get("/api/meetings/:uid", async (req, res) => {
-  const uid = req.params.uid;
-  const meetings = await prisma.meeting.findMany({
-    where: {
-      authorId: uid,
-    },
-  });
-  // getした情報を並び替える;
-  for (let outer = 0; outer < meetings.length - 1; outer++) {
-    for (let i = meetings.length - 1; i > outer; i--) {
-      if (meetings[i].id < meetings[i - 1].id) {
-        let tmp = meetings[i];
-        meetings[i] = meetings[i - 1];
-        meetings[i - 1] = tmp;
-      }
-    }
-  }
-  console.log("🙇‍♀️", meetings);
-  res.json(meetings);
-});
+app.post("/api/user/:uid", postNewUser);
+app.post("/api/meetings/:uid", postMeeting);
 
-app.post("/api/meetings/:uid", async (req, res) => {
-  const data = req.body;
-  const uid = req.params.uid;
+app.put("/api/meetings/:uid", putMeeting);
 
-  let count = 1;
-  const agendas = data.agendas.map((agenda) => {
-    agenda.order = count;
-    count++;
-    return agenda;
-  });
-  console.log(agendas);
-
-  let newMeeting = {
-    title: data.title,
-    author: {
-      connect: {
-        uid: uid,
-      },
-    },
-    agendas: {
-      create: agendas,
-    },
-  };
-  console.log(newMeeting);
-  const createMeeting = await prisma.meeting.create({ data: newMeeting });
-  res.json(newMeeting);
-});
-
-app.delete("/api/meetings/:uid", async (req, res) => {
-  const meetingId = Number(req.query.meetingId);
-  console.log("👧", meetingId);
-
-  const deleteAgenda = await prisma.agenda.deleteMany({
-    where: {
-      meetingId: meetingId,
-    },
-  });
-  const deleteMeeting = await prisma.meeting.delete({
-    where: {
-      id: meetingId,
-    },
-  });
-
-  res.json(deleteAgenda);
-});
-
-app.put("/api/meetings/:uid", async (req, res) => {
-  const meetingId = Number(req.query.meetingId);
-  const data = req.body;
-
-  let count = 1;
-  const agendas = data.agendas.map((agenda) => {
-    agenda.order = count;
-    count++;
-    return agenda;
-  });
-
-  console.log("😭", agendas);
-
-  const deleteAgenda = await prisma.agenda.deleteMany({
-    where: {
-      meetingId: meetingId,
-    },
-  });
-
-  const putMeeting = await prisma.meeting.update({
-    where: {
-      id: meetingId,
-    },
-    data: { title: data.title },
-  });
-
-  data.agendas.map(async (agenda) => {
-    const createNewAgenda = await prisma.agenda.create({
-      data: {
-        title: agenda.title,
-        time: agenda.time,
-        order: agenda.order,
-        meeting: {
-          connect: {
-            id: meetingId,
-          },
-        },
-      },
-    });
-    console.log("🐱", createNewAgenda);
-  });
-
-  res.json(putMeeting);
-});
-
-app.get("/api/agendas/:uid", async (req, res) => {
-  const meetingId = Number(req.query.meetingId);
-
-  const meetingInfo = await prisma.meeting.findUnique({
-    where: {
-      id: meetingId,
-    },
-  });
-
-  if (meetingInfo) {
-    let agendas = await prisma.agenda.findMany({
-      where: {
-        meetingId: meetingId,
-      },
-    });
-
-    if (agendas.length > 0) {
-      console.log("🐵", agendas);
-      // getした情報を並び替える;
-      for (let outer = 0; outer < agendas.length - 1; outer++) {
-        for (let i = agendas.length - 1; i > outer; i--) {
-          if (agendas[i].order < agendas[i - 1].order) {
-            let tmp = agendas[i];
-            agendas[i] = agendas[i - 1];
-            agendas[i - 1] = tmp;
-          }
-        }
-      }
-      const resData = { title: meetingInfo.title, agendas: agendas };
-      console.log(resData);
-      res.status(200).json(resData);
-    }
-  }
-  if (!meetingInfo) {
-    console.log("違う");
-    res.status(403).json("not auther");
-  }
-});
+app.delete("/api/meetings/:uid", deleteMeeting);
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
